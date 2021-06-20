19c47a3cd8bfc1c8f70a3ffe02e228c3261f9c19
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-06-20 22:17:17 +0200

    Add a testing json with some placeholder data, initial javascript for reading from the json file (check console).

---
+++ main.js
@@ -0,0 +1,10 @@
+
+fetch("/downloads.json").then(data => data.json().then(downloads => {
+	productList = downloads;
+	for (const i in downloads) {
+		console.log(i);
+		for (const j in downloads[i]) {
+			console.log(j);
+		}
+	}
+}));
