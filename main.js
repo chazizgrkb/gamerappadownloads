632f3d28c7b97a65469cf1192bfd4441c36c32e4
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-06-20 22:53:35 +0200

    make json path relative

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-fetch("/downloads.json").then(data => data.json().then(downloads => {
+fetch("downloads.json").then(data => data.json().then(downloads => {
 	productList = downloads;
 	for (const i in downloads) {
 		console.log(i);
